be3d17f24e80195c8affe87d6f77a0cafa21ee6c
TylerWallace45 <tylerawallace45@gmail.com>
2026-04-14 09:51:12 -0700

    Update build.gradle to include new tasks and initialize leader/worker file

---
+++ src/main/java/Leader.java
@@ -0,0 +1,9 @@
+public class Leader {
+    public Leader() {
+        System.out.println("Hello from the Leader!");
+    }
+
+    public static void main(String[] args) {
+        Leader leader = new Leader();
+    }
+}
---
+++ src/main/java/Worker.java
@@ -0,0 +1,9 @@
+public class Worker {
+	public Worker() {
+        System.out.println("Hello from the worker!");
+    }
+
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+    }
+}
